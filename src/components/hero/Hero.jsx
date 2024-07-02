9fedcd80051334412001e07ddcf61dbca44a6556
Gifty <giftyaningg@gmail.com>
2024-07-02 21:31:51 +0100

    Hero structure

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import '../../styles/hero.css'
+import Button from '../buttons/Button'
+import Bg from '../../images/Bg-intro-desktop.svg'
+
 
 const Hero = () => {
   return (
-    <div>
+    <div className='hero-section'>
+      <div className="description">
+        <h1>Next generation digital banking</h1>
+        <p>
+          Take your financial life online. Your Easybank
+          account will be a one-stop-shop for spending, 
+          saving, budgeting, investing, and much more.
+        </p>
+        <Button />
+      </div>
+      
+        <img src={Bg} alt="" className='background-image' />
       
     </div>
   )
